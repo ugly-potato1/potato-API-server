46dca28d5a04d8029eda92332a8e7ca352bece38
HearHere <jsw5913@naver.com>
2023-08-08 14:55:58 +0900

    feat: 주문 관련 도메인 엔티티 정의

---
+++ src/main/java/potato/potatoAPIserver/order/domain/Dlivery.java
@@ -0,0 +1,43 @@
+package potato.potatoAPIserver.order.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import potato.potatoAPIserver.common.BaseTimeEntity;
+
+/**
+ * @author 정순원
+ * @since 2023-08-08
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class Dlivery extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "dliver_id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(length = 50, nullable = false)
+    private String dliveryStatus;
+
+    private int orderNumber;
+
+    @Column(length = 100)
+    private String shipper;
+
+    @Builder
+    public Dlivery(Order order, String dliveryStatus, int orderNumber, String shipper) {
+        this.order = order;
+        this.dliveryStatus = dliveryStatus;
+        this.orderNumber = orderNumber;
+        this.shipper = shipper;
+    }
+}
---
+++ src/main/java/potato/potatoAPIserver/order/domain/ReviewImage.java
@@ -0,0 +1,35 @@
+package potato.potatoAPIserver.order.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import potato.potatoAPIserver.common.BaseTimeEntity;
+
+/**
+ * @author 정순원
+ * @since 2023-08-08
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class ReviewImage extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(length = 500, nullable = false)
+    private String link;
+
+    @Builder
+    public ReviewImage(Review review, String link) {
+        this.review = review;
+        this.link = link;
+    }
+}
