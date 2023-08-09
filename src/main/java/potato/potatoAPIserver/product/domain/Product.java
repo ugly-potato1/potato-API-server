1f9d98cfb546a921319bdeb26f1779f5eca1183e
Geonhwi Park <geonhwipark715@gmail.com>
2023-08-09 19:58:56 +0900

    Feat: 상품 관련 도메인 엔티티 정의 (#5)

---
+++ src/main/java/potato/potatoAPIserver/product/domain/Product.java
@@ -0,0 +1,48 @@
+package potato.potatoAPIserver.product.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import potato.potatoAPIserver.common.BaseTimeEntity;
+
+/**
+ * @author 박건휘
+ * @since 2023-08-09
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "product")
+public class Product extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(length = 1000, nullable = false)
+    private String description;
+
+    private int hit;
+
+    @Builder
+    public Product(Category category, String title, int price, String description, int hit) {
+        this.category = category;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+        this.hit = hit;
+    }
+}
