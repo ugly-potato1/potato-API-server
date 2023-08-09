810419b44d5abf37d33ad3e0d7aa8b7954ce1789
sunwon <92251131+sunwon12@users.noreply.github.com>
2023-08-09 15:24:36 +0900

    Feat: 주문 관련 도메인 정의 (#3)
    * feat: BaseTimeEntity 엔티티 정의
    
    * feat: 주문 관련 도메인 엔티티 정의
    
    * chore: .gitignore 트래킹 안되게 설정
    
    * fix: 배송엔티티 이름 수정
    
    * Fix: 배송엔티티 수정2

---
+++ src/main/java/potato/potatoAPIserver/order/domain/Order.java
@@ -0,0 +1,35 @@
+package potato.potatoAPIserver.order.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import potato.potatoAPIserver.common.BaseTimeEntity;
+
+/**
+ * @author 정순원
+ * @since 2023-08-08
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private int orderPrice;
+
+    @Builder
+    public Order(User user, int orderPrice) {
+        this.user = user;
+        this.orderPrice = orderPrice;
+    }
+}
