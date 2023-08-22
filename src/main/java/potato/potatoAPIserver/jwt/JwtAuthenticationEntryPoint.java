13abdd4e1bc171c4592b309de2579e78460185bb
HearHere <jsw5913@naver.com>
2023-08-22 14:41:24 +0900

    feat: jwt관련 클래스 정의

---
+++ src/main/java/potato/potatoAPIserver/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package potato.potatoAPIserver.jwt;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * jwt 인증 실패하면 처리하는 클래스
+ * security config에 추가하셔도 되고
+ * 안 쓰실 거면 삭제하셔도 됩니다
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+
+    }
+}
+
