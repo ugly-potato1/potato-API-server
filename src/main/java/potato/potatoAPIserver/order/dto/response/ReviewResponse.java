3c77401d91bb5d41ef0473e918b4f8f3cd896be0
sunwon <92251131+sunwon12@users.noreply.github.com>
2023-10-16 15:42:45 +0900

    Feat: 공통 ResponseForm 구현 (#17)

---
+++ src/main/java/potato/potatoAPIserver/order/dto/response/ReviewResponse.java
@@ -0,0 +1,10 @@
+package potato.potatoAPIserver.order.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReviewResponse {
+    private long reviewId;
+}
