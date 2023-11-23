d1d17fc64a922b5a3072100ca244f959d366dfa5
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-23 14:27:15 +0900

    #39 - feat: 카트 상품 조회 기능 구현

--- src/main/java/potato/potatoAPIserver/cart/repository/CartProductRepository.java
+++ src/main/java/potato/potatoAPIserver/cart/repository/CartProductRepository.java
@@ -1,10 +1,12 @@
 package potato.potatoAPIserver.cart.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import potato.potatoAPIserver.cart.domain.CartProduct;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,4 +17,7 @@ public interface CartProductRepository extends JpaRepository<CartProduct, Long> 
 
     @Query("SELECT cp FROM CartProduct cp WHERE cp.cart.id = :cartId AND cp.product.id = :productId")
     Optional<CartProduct> findCartProduct(@Param("cartId") Long cartId, @Param("productId") Long productId);
+
+    @EntityGraph(attributePaths = {"product"})
+    List<CartProduct> findAllByCartId(@Param("cartId") Long cartId);
 }
