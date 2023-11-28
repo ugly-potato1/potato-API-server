0251caeb7e8b4c39af96cab87a7e07d940eeb5d6
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-28 19:52:50 +0900

    #13 - test: 실패하는 테스트 수정

--- src/test/java/potato/potatoAPIserver/fixture/CartFixture.java
+++ src/test/java/potato/potatoAPIserver/fixture/CartFixture.java
@@ -1,10 +1,12 @@
 package potato.potatoAPIserver.fixture;
 
+import potato.potatoAPIserver.cart.domain.Cart;
 import potato.potatoAPIserver.cart.domain.CartProduct;
 
 import java.lang.reflect.Field;
 
 import static potato.potatoAPIserver.fixture.ProductFixture.createProduct;
+import static potato.potatoAPIserver.fixture.UserFixture.*;
 
 public class CartFixture {
     public static CartProduct creatCartProduct(Long id) throws NoSuchFieldException, IllegalAccessException {
@@ -18,5 +20,17 @@ public class CartFixture {
         idField.set(cartProduct, id);
 
         return cartProduct;
+    }
+
+    public static Cart createCart(Long id) throws NoSuchFieldException, IllegalAccessException {
+        Cart cart = Cart.builder()
+                .user(createUser(1L))
+                .build();
+
+        Field idField = Cart.class.getDeclaredField("id");
+        idField.setAccessible(true);
+        idField.set(cart, id);
+
+        return cart;
     }
 }
