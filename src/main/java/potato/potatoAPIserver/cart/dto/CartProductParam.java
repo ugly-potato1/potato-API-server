9b7a235af457854e714a11dc7be322951ec7582f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-11-28 19:14:31 +0900

    #13 - refactor: dto 를 param 으로 변경

--- src/main/java/potato/potatoAPIserver/cart/dto/CartProductParam.java
+++ src/main/java/potato/potatoAPIserver/cart/dto/CartProductParam.java
@@ -3,16 +3,16 @@ package potato.potatoAPIserver.cart.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import potato.potatoAPIserver.cart.domain.CartProduct;
-import potato.potatoAPIserver.product.dto.ProductDto;
+import potato.potatoAPIserver.product.dto.ProductParam;
 
 @Getter
 @AllArgsConstructor
 public class CartProductParam {
     private Long id;
-    private ProductDto product;
+    private ProductParam product;
     private int quantity;
 
     public static CartProductParam from(CartProduct entity) {
-        return new CartProductParam(entity.getId(), ProductDto.from(entity.getProduct()), entity.getQuantity());
+        return new CartProductParam(entity.getId(), ProductParam.from(entity.getProduct()), entity.getQuantity());
     }
 }
