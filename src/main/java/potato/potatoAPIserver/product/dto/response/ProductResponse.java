4fc7dbea8db4ec7ebb9faf4d1b83dabf1028344d
Geonhwi Park <ghpark715@gmail.com>
2023-11-29 14:43:02 +0900

    Update ProductResponse.java

--- src/main/java/potato/potatoAPIserver/product/dto/response/ProductResponse.java
+++ src/main/java/potato/potatoAPIserver/product/dto/response/ProductResponse.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.math.BigDecimal;
+
 /**
  * @author: 박건휘
  * @since: 2023-11-13
@@ -13,15 +15,21 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ProductResponse {
     private String title;
-    private Integer price;
+    private BigDecimal price;
     private String description;
+    private Integer hit;
+    private Integer stock;
+    private Integer version;
 
-    public static ProductResponse of(String title, Integer price, String description) {
+    public static ProductResponse of(String title, BigDecimal price, String description, Integer stock, Integer version, Integer hit) {
 
         return ProductResponse.builder()
                 .title(title)
                 .price(price)
                 .description(description)
+                .stock(stock)
+                .version(version)
+                .hit(hit)
                 .build();
     }
 
