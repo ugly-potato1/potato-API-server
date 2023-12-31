f6965608d0f6620eb2159c6539333bfc5a343f4b
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-12-31 20:28:03 +0900

    #25 - test: 주문 취소 테스트 작성

--- src/test/java/potato/server/order/service/OrderWriteServiceTest.java
+++ src/test/java/potato/server/order/service/OrderWriteServiceTest.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import potato.server.cart.domain.Cart;
 import potato.server.cart.domain.CartProduct;
 import potato.server.cart.repository.CartProductRepository;
 import potato.server.cart.service.CartReadService;
+import potato.server.common.CustomException;
+import potato.server.common.ResultCode;
 import potato.server.order.domain.Order;
 import potato.server.order.dto.request.OrderCreateRequest;
 import potato.server.order.repository.OrderRepository;
+import potato.server.user.domain.User;
 import potato.server.user.service.UserService;
 
 import java.util.Arrays;
@@ -20,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.*;
 import static potato.server.fixture.CartFixture.creatCartProduct;
@@ -76,6 +81,64 @@ class OrderWriteServiceTest {
         assertThat(orderId).isNotNull().isEqualTo(1L); // 생성된 ID가 예상대로 반환되었는지 확인
     }
 
+    @DisplayName("주문을 삭제한다.")
+    @Test
+    void deleteOrder() {
+        Long userId = 1L;
+        Long orderId = 2L;
+
+        User user = User.builder().build();
+        given(userService.getUserById(userId)).willReturn(user);
+
+        Order order = Order.builder().user(user).build();
+        given(orderRepository.findById(orderId)).willReturn(Optional.of(order));
+
+        sut.deleteOrder(userId, orderId);
+
+        then(userService).should().getUserById(userId);
+        then(orderRepository).should().findById(orderId);
+        then(orderRepository).should().delete(order);
+
+        assertThat(order.getUser()).isEqualTo(user);
+    }
+
+    @DisplayName("잘못된 유저의 주문 삭제시 에러를 반환한다.")
+    @Test
+    void deleteOrderReturnErrorWithInvalidUser() {
+        Long userId = 1L;
+        Long orderId = 2L;
+
+        User user = User.builder().build();
+        given(userService.getUserById(userId)).willReturn(user);
+
+        Order order = Order.builder().build();
+        given(orderRepository.findById(orderId)).willReturn(Optional.of(order));
+
+        assertThatThrownBy(() -> sut.deleteOrder(userId, orderId))
+                .isInstanceOf(CustomException.class)
+                .hasMessage("해당 유저의 주문이 아닙니다.")
+                .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.FORBIDDEN)
+                .hasFieldOrPropertyWithValue("resultCode", ResultCode.AUTH_USER_NOT);
+    }
+
+    @DisplayName("존재하지 않는 주문 삭제시")
+    @Test
+    void deleteInvalidOrderReturnError() {
+        Long userId = 1L;
+        Long orderId = 2L;
+
+        User user = User.builder().build();
+        given(userService.getUserById(userId)).willReturn(user);
+
+        Order order = Order.builder().build();
+        given(orderRepository.findById(orderId)).willReturn(Optional.empty());
+
+        assertThatThrownBy(() -> sut.deleteOrder(userId, orderId))
+                .isInstanceOf(CustomException.class)
+                .hasMessage("존재하지 않는 주문입니다.")
+                .hasFieldOrPropertyWithValue("httpStatus", HttpStatus.NOT_FOUND)
+                .hasFieldOrPropertyWithValue("resultCode", ResultCode.ORDER_NOT_FOUND);
+    }
 
 
 
