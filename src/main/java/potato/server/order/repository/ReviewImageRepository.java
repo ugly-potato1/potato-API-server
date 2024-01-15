4e23addf3b3fd53862f16080d960edd78503bec9
sunwon <jsw5913@naver.com>
2024-01-15 18:46:07 +0900

    chore: 리뷰 도메인 주문에서 분리

--- src/main/java/potato/server/order/repository/ReviewImageRepository.java
+++
@@ -1,7 +0,0 @@
-package potato.server.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import potato.server.order.domain.ReviewImage;
-
-public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
-}
