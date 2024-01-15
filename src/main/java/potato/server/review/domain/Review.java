54ec710c6d56e8fa71e5ab0c506d9d473c7acfbc
sunwon <jsw5913@naver.com>
2024-01-15 18:50:51 +0900

    feat: 리뷰 이미지 생성, 삭제 api

---
+++ src/main/java/potato/server/review/domain/Review.java
@@ -0,0 +1,52 @@
+package potato.server.review.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import potato.server.common.BaseTimeEntity;
+import potato.server.product.domain.Product;
+import potato.server.user.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 정순원
+ * @since 2023-08-08
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(length = 500, nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private int evaluation;
+
+    @OneToMany(mappedBy = "review")
+    private List<ReviewImage> reviewImages = new ArrayList<>();
+
+    @Builder
+    public Review(Product product, User user, String content, int evaluation) {
+        this.product = product;
+        this.user = user;
+        this.content = content;
+        this.evaluation = evaluation;
+    }
+}
---
+++ src/main/java/potato/server/review/dto/request/ReviewCreateRequest.java
@@ -0,0 +1,24 @@
+package potato.server.review.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @Author 정순원
+ * @Since 2023-10-12
+ */
+@Data
+@NoArgsConstructor
+public class ReviewCreateRequest {
+
+    @NotNull
+    private Long productId;
+    private String content;
+    @NotNull
+    private int evaluation;
+    private List<Long> reviewImageIds = new ArrayList<>();
+}
