5e150e059bd70d065b4c73cc40b9a624168adf99
sunwon <jsw5913@naver.com>
2024-01-16 21:34:10 +0900

    feat: 특정 상품의 후기 조회 API

--- src/main/java/potato/server/review/repository/ReviewRepository.java
+++ src/main/java/potato/server/review/repository/ReviewRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import potato.server.review.domain.Review;
 
+import java.util.List;
 /**
  * @Author 정순원
  * @Since 2023-10-12
@@ -15,5 +16,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT COUNT(r) > 0 FROM Review r WHERE r.user.id = :userId AND r.id = :reviewId")
     boolean existsByUserIdAndReviewId(@Param("userId") long userId, @Param("reviewId") long reviewId);
+
+    @Query("SELECT r FROM Review r JOIN FETCH r.user WHERE r.product.id = :productId")
+    List<Review> findByProductId(@Param("productId") Long productId);
 }
 
