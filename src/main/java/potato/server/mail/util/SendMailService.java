fada195edd5b798eeb69f0e2f1a15f6f5fa0b06a
sunwon <jsw5913@naver.com>
2024-01-16 16:41:33 +0900

    feat: 인증이메일 전송 비동기 처리

--- src/main/java/potato/server/mail/util/SendMailService.java
+++ src/main/java/potato/server/mail/util/SendMailService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,6 +21,7 @@ public class SendMailService {
     @Value("${email.id}")
     private String fromId;
 
+    @Async("mailExecutor")
     public void sendEmail(String to, String subject, String content) {
         MimeMessagePreparator messagePreparator =
                 mimeMessage -> {
